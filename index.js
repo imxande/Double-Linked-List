672b1893b2e90f2a2f1ff63bc0e62a996475cb32
imxande <alerodriguezlarosa@gmail.com>
2020-07-03 16:40:36 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  setHead(node) {}
+
+  setTail(node) {}
+
+  insertBefore(node, nodeToInsert) {}
+
+  insertAfter(node, nodeToInsert) {}
+
+  insertAtPosition(position, nodeToInsert) {}
+
+  removeNodesWithValue(value) {}
+
+  remove(node) {}
+
+  containsNodeWithValue(value) {}
+}
