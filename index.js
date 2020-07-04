4a6ac2b68f8c5a27db703780ed61c58d5ee60fb9
imxande <alerodriguezlarosa@gmail.com>
2020-07-04 17:05:09 -0400

    completed implementation

--- index.js
+++ index.js
@@ -5,26 +5,125 @@ class Node {
     this.next = null;
   }
 }
-
 class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
   }
 
-  setHead(node) {}
+  setHead(node) {
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      return;
+    }
 
-  setTail(node) {}
+    this.insertBefore(this.head, node);
+  }
 
-  insertBefore(node, nodeToInsert) {}
+  setTail(node) {
+    if (this.tail === null) {
+      this.setHead(node);
+      return;
+    }
+    this.insertAfter(this.tail, node);
+  }
 
-  insertAfter(node, nodeToInsert) {}
+  insertBefore(node, nodeToInsert) {
+    if (nodeToInsert === this.head && nodeToInsert === this.tail) {
+      return;
+    }
+    this.remove(nodeToInsert);
+    nodeToInsert.prev = node.prev;
+    nodeToInsert.next = node;
 
-  insertAtPosition(position, nodeToInsert) {}
+    if (node.prev === null) {
+      this.head = nodeToInsert;
+    } else {
+      node.prev.next = nodeToInsert;
+    }
+    node.prev = nodeToInsert;
+  }
 
-  removeNodesWithValue(value) {}
+  insertAfter(node, nodeToInsert) {
+    if (nodeToInsert === this.head && nodeToInsert === this.tail) {
+      return;
+    }
+    this.remove(nodeToInsert);
+    nodeToInsert.prev = node;
+    nodeToInsert.next = node.next;
 
-  remove(node) {}
+    if (node.next === null) {
+      this.tail = nodeToInsert;
+    } else {
+      node.next.prev = nodeToInsert;
+    }
 
-  containsNodeWithValue(value) {}
+    node.next = nodeToInsert;
+  }
+
+  insertAtPosition(position, nodeToInsert) {
+    if (position === 1) {
+      this.setHead(nodeToInsert);
+      return;
+    }
+    let node = this.head;
+    let currentPosition = 1;
+
+    while (node !== null && currentPosition++ !== position) {
+      node = node.next;
+    }
+    if (node !== null) {
+      this.insertBefore(node, nodeToInsert);
+    } else {
+      this.setTail(nodeToInsert);
+    }
+  }
+
+  removeNodesWithValue(value) {
+    let node = this.head;
+
+    while (node !== null) {
+      let nodeToRemove = node;
+      node = node.next;
+
+      if (nodeToRemove.value === value) {
+        this.remove(nodeToRemove);
+      }
+    }
+  }
+
+  remove(node) {
+    if (node === this.head) {
+      this.head = this.head.next;
+    }
+    if (node === this.tail) {
+      this.tail = this.tail.prev;
+    }
+    this.removeNodeBindings(node);
+  }
+
+  removeNodeBindings(node) {
+    if (node.prev !== null) {
+      node.prev.next = node.next;
+    }
+    if (node.next !== null) {
+      node.next.prev = node.prev;
+    }
+    node.prev = null;
+    node.next = null;
+  }
+
+  containsNodeWithValue(value) {
+    // get access to the head
+    let node = this.head;
+
+    // check if linked list is empty and if the value is not the head value
+    while (node !== null && node.value !== value) {
+      // traverse the linked list
+      node = node.next;
+    }
+
+    return node !== null;
+  }
 }
